39e471e5aa6e1efff1a463e63f90f1eaa429b3b7
Danil22May <142904608+Danil22May@users.noreply.github.com>
2025-01-30 19:42:44 +0100

    Heroe Card updated

--- src/components/HeroeCard.jsx
+++ src/components/HeroeCard.jsx
@@ -1,15 +1,19 @@
 export default function HeroeCard({ hero }) {
   return (
-    <div className="bg-neutral-100 border m-2 w-60 h-40 flex flex-col gap-4 border-neutral-200 shadow-sm hover:shadow-md rounded p-2">
-      <div>
-        Name: <span className="text-blue-600 font-bold">{hero.name}</span>{" "}
+    <div className="bg-neutral-100 border m-2 w-64 h-40 flex flex-col gap-4 border-neutral-200 shadow-sm hover:shadow-md rounded p-2">
+      <div className="text-sm">
+        Name:{" "}
+        <span className="text-lg text-blue-600 font-bold">{hero.name}</span>{" "}
       </div>
-      <div>
-        Humble Score: <span className="font-bold">{hero.humbleScore}</span>
+      <div className="text-sm">
+        Humble Score:{" "}
+        <span className="text-lg font-bold">{hero.humbleScore}</span>
       </div>
-      <div>
+      <div className="text-sm">
         Super Power:{" "}
-        <span className="text-yellow-600 font-bold">{hero.superPower}</span>
+        <span className="text-lg text-yellow-600 font-bold">
+          {hero.superPower}
+        </span>
       </div>
     </div>
   );
